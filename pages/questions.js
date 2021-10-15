e490924143c4766f7aa7274c44b470a085393701
iamrosada <luisrosada2018@gmail.com>
2021-10-15 18:51:34 +0300

    Created Page to show questions

--- pages/questions.js
+++ pages/questions.js
@@ -1,24 +1,5 @@
-import { gql, useMutation } from "@apollo/client";
 import CreateQuestions from "../components/Questions/question";
 
-const CREATE_QUESTION_MUTATION = gql`
-  mutation createQuestionMutation($input: HostQuestionCreateMutationInput!) {
-    questionCreate(input: $input) {
-      errors
-      question {
-        id
-        content
-      }
-    }
-  }
-`;
-
 export default function Questions() {
-  /*const [questionCreate, { loading, error, data }] = useMutation(
-    CREATE_QUESTION_MUTATION
-  );
-  if (loading) return <p>Loading</p>;
-  if (error) return console.log("the error is ", error);*/
-
   return <CreateQuestions />;
 }
